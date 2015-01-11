360318c464a3bb231c5be7ac159db7a0ab972de5
tknemuru <kojikoji.max.5252@gmail.com>
2015-01-12 00:14:32 +0900

    sp complete

---
+++ scripts/app.js
@@ -0,0 +1,18 @@
+﻿$(function () {
+    $('a[href^=#]').click(function () {
+        var speed = 500;
+        var href = $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top;
+        $("html, body").animate({ scrollTop: position }, speed, "swing");
+        return false;
+    });
+
+    $('.sp-menu').click(
+        function () {
+            $('.sp-menu-first').slideToggle(200);
+            $('.sp-menu-second').slideToggle(200);
+            $('.social-link-container').slideToggle(200);
+        }
+    );
+});
