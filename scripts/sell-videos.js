70e2d98f74140bad2cfe54becd15857316e1c772
tknemuru <kojikoji.max.5252@gmail.com>
2015-03-29 16:13:17 +0900

    sell video

---
+++ scripts/sell-videos.js
@@ -0,0 +1,32 @@
+﻿$(function () {
+    var setTestData = function() {
+        $('#seller-name').val('山田　太郎');
+        $('#seller-email').val('yamada.taro@example.com');
+        $('#seller-phone-number').val('99-9999-9999');
+        $('#video-url').val('http://example.com');
+        $('#video-explain').val('これは\r\n動画説明文\r\nのテストです。');
+    }
+    setTestData();
+    
+    $('#form-input-container').submit(function (event) {
+        event.preventDefault();
+
+        var $form = $(this);
+        console.log($form.serialize());
+
+        $.ajax({
+            url: $form.attr('action'),
+            type: $form.attr('method'),
+            data: $form.serialize(),
+            timeout: 10000,
+            success: function(result, textStatus, xhr) {
+                console.log(result);
+                alert('send success!');
+            },
+            error: function(xhr, textStatus, error) {
+                console.log(error);
+                alert('send error!');
+            }
+        });
+    });
+});
