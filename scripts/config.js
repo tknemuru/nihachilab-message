2ac699967df22b228628b791cb9b70fe2bab7084
tknemuru <kojikoji.max.5252@gmail.com>
2015-08-30 10:50:44 +0900

    mod

--- scripts/config.js
+++ scripts/config.js
@@ -6,8 +6,10 @@
     var config = (function () {
          function config() {
             this.api = {
-                baseUrl: '//192.168.33.10:5000/',
-                agreementRegister: '//192.168.33.10:5000/agreement_register/'
+                //baseUrl: '//192.168.33.10:5000/',
+                //agreementRegister: '//192.168.33.10:5000/agreement_register/'
+                baseUrl: 'https://nihachilab-transfer.herokuapp.com/',
+                agreementRegister: 'https://nihachilab-transfer.herokuapp.com/agreement_register/'
             };
             this.queryString = {
                 agreementedId: 'id'
