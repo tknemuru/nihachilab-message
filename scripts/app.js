a646b274588cd45a6beb1af819e75b55b91d4526
tknemuru <kojikoji.max.5252@gmail.com>
2015-12-06 19:22:24 +0900

    dec mod

--- scripts/app.js
+++ scripts/app.js
@@ -15,4 +15,31 @@
             $('.social-link-container').slideToggle(200);
         }
     );
+
+    // コンテンツを英語に切り替えます。
+    var toggleToEn = function () {
+        $('.content-en').show();
+        $('.content-ja').hide();
+        $('#toggle-en').hide();
+        $('#toggle-ja').show();
+    };
+
+    // コンテンツを日本語に切り替えます。
+    var toggleToJa = function () {
+        $('.content-en').hide();
+        $('.content-ja').show();
+        $('#toggle-en').show();
+        $('#toggle-ja').hide();
+    };
+
+    $('#toggle-en').click(function () {
+        toggleToEn();
+    });
+
+    $('#toggle-ja').click(function () {
+        toggleToJa();
+    });
+
+    // on load
+    toggleToJa();
 });
